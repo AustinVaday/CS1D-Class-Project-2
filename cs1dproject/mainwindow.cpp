eb492e58660dca9bf485e6d1b132ee5ab22c1291
Orbotron <ErikDKarlsson8@gmail.com>
2015-05-11 10:24:39 -0700

    Saving work

--- cs1dproject/mainwindow.cpp
+++ cs1dproject/mainwindow.cpp
@@ -1,6 +1,7 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include <QErrorMessage>
+#include <QDataWidgetMapper>
 #include <QModelIndex>
 #include <QMessageBox>
 #include <QModelIndexList>
@@ -161,8 +162,12 @@ void MainWindow::on_button_back4_clicked()
 
 void MainWindow::on_button_viewStadiums_clicked()
 {
-	QTableView* tableView = createView(initModel, "Stadiums");
-	tableView->show();
+	QDataWidgetMapper *mapper = new QDataWidgetMapper(initModel);
+//	mapper->setModel(initModel);
+//	mapper->addMapping();
+//	QVBoxLayout *layout = new QVBoxLayout;
+
+
 	ui->page_mainMenu->hide();
 	ui->page_stadiumList->show();
 	refresh();
@@ -310,35 +315,149 @@ bool MainWindow::createConnection(bool restart)
 							   "Park;1255~Yankee Stadium;195')");
 		qDebug() << query.exec("insert into stadiums values"
 							   "(4, 'Globe Life Park in Arlington', "
-							   "'Texas Rangers', '1000 Ballpark Way', 'Arlington', 'TX', '76011', '817-273-5222', '1994-04-1', '48,114', 'American', 'Grass', 'Chase Field;870~Coors Field;650~Kauffman Stadium;460~Turner Field;740~Minute Maid Park;230')");
-		qDebug() << query.exec("insert into stadiums values(5, 'Kauffman Stadium', 'Kansas City Royals', '1 Royal Way', 'Kansas City', 'MO', '64129', '816-921-8000', '1973-04-10', '37,903', 'American', 'Grass', 'Wrigley Field;415~Busch Stadium;235~Globe Life Park in Arlington;460~Coors Field;560')");
-		qDebug() << query.exec("insert into stadiums values(6, 'Minute Maid Park', 'Houston Astros', '501 Crawford St', 'Houston', 'TX', '77002', '713-259-8000', '2000-03-30', '42,060', 'American', 'Grass', 'Busch Stadium;680~Tropicana Field;790~Marlins Park;965~Chase Field;1115~Globe Life Park in Arlington;230')");
-		qDebug() << query.exec("insert into stadiums values(7, 'O.co Coliseum', 'Oakland Athletics', '7000 Coliseum Way', 'Oakland', 'CA', '94621', '510-569-2121', '1966-09-18', '37,090 (April - August) & 55,945 (Sept - Jan)', 'American', 'Grass', 'AT&T Park;0~Chase Field;650~Dodger Stadium;340')");
-		qDebug() << query.exec("insert into stadiums values(8, 'Oriole Park at Camden Yards', 'Baltimore Orioles', '333 West Camden Street', 'Baltimore', 'MD', '21201', '410-685-9800', '1992-04-6', '48,187', 'American', 'Grass', 'Nationals Park;0~Citizens Bank Park;90')");
-		qDebug() << query.exec("insert into stadiums values(9, 'Progressive Field', 'Cleveland Indians', '2401 Ontario Street', 'Cleveland', 'OH', '44115', '216-420-4487', '1994-04-2', '42,404', 'American', 'Grass', 'Comerica Park;90~PNC Park;115~Great American Ball Park;225')");
-		qDebug() << query.exec("insert into stadiums values(10, 'Rogers Centre', 'Toronto Blue Jays', '1 Blue Jays Way', 'Toronto', 'Ontario,Canada', 'M5V1J3', '416-341-1000', '1989-06-3', '49,282', 'American', 'Turf', 'Fenway Park;430~PNC Park;225~Comerica Park;210~Miller Park;430~SafeCo Field;2070')");
-		qDebug() << query.exec("insert into stadiums values(11, 'SafeCo Field', 'Seattle Mariners', '1516 First Avenue South', 'Seattle', 'WA', '98134', '206-346-4000', '1999-07-15', '47,476', 'American', 'Grass', 'Rogers Centre;2070~Target Field;1390~AT&T Park;680')");
-		qDebug() << query.exec("insert into stadiums values(12, 'Target Field', 'Minnesota Twins', '353 N 5th St', 'Minneapolis', 'MN', '55403', '800-338-9467', '2010-04-12', '39,021', 'American', 'Grass', 'SafeCo Field;1390~Miller Park;300~Busch Stadium;465~Dodger Stadium;1500')");
-		qDebug() << query.exec("insert into stadiums values(12, 'Tropicana Field', 'Tampa Bay Rays', '1 Tropicana Dr', 'St. Petersburg', 'FL', '33705', '727-825-3137', '1990-03-3', '31,042 (Regular Season) 42,735 (Postseason)', 'American', 'Turf', 'Marlins Park;210~Minute Maid Park;790~Great American Ball Park;790')");
-		qDebug() << query.exec("insert into stadiums values(13, 'US Cellular Field', 'Chicago White Sox', '333 West 35th Street', 'Chicago', 'IL', '60616', '312-674-1000', '1991-04-18', '40,615', 'American', 'Grass', 'Wrigley Field;0~Comerica Park;240~Great American Ball Park;250')");
-		qDebug() << query.exec("insert into stadiums values(14, 'Yankee Stadium', 'New York Yankees', '1 E 161st St', 'South Bronx', 'NY', '10451', '718-293-4300', '2009-04-16', '50,291', 'American', 'Grass', 'Citi Field;0~Citizens Bank Park;80~PNC Park;315~Fenway Park;195')");
-		qDebug() << query.exec("insert into stadiums values(15, 'AT&T Park', 'San Francisco Giants', '24 Willie Mays Plaza', 'San Francisco', 'CA', '94107', '415-972-2000', '2000-04-11', '41,915', 'National', 'Grass', 'O.co Coliseum;0~SafeCo Field;680')");
-		qDebug() << query.exec("insert into stadiums values(16, 'Busch Stadium', 'St. Louis Cardinals', '700 Clark Street', 'St. Louis', 'MO', '63102', '314-345-9600', '2006-04-10', '46,861', 'National', 'Grass', 'Great American Ball Park;310~Minute Maid Park;680~Kauffman Stadium;235~Target Field;465')");
-		qDebug() << query.exec("insert into stadiums values(17, 'Chase Field', 'Arizona Diamondbacks', '401 East Jefferson Street', 'Phoenix', 'AZ', '85004', '602-462-6500', '1998-03-31', '48,633', 'National', 'Grass', 'Coors Field;580~Globe Life Park  in Arlington;870~Minute Maid Park;1115~Petco Park;300~O.co Coliseum;650')");
-		qDebug() << query.exec("insert into stadiums values(18, 'Citi Field', 'New York Mets', '126th St. & Roosevelt Ave.', 'Queens', 'NY', '11368', '718-507-6387', '2009-04-13', '41,922', 'National', 'Grass', 'Yankee Stadium;0')");
-		qDebug() << query.exec("insert into stadiums values(19, 'Citizens Bank Park', 'Philadelphia Phillies', '1 Citizens Bank Way', 'Philadelphia', 'PA', '19148', '215-463-1000', '2004-04-3', '43,651', 'National', 'Grass', 'Yankee Stadium;80~Oriole Park at Camden Yards;90')");
-		qDebug() << query.exec("insert into stadiums values(20, 'Coors Field', 'Colorado Rockies', '2001 Blake St', 'Denver', 'CO', '80205', '303-292-0200', '1995-04-26', '50,480', 'National', 'Grass', 'Kauffman Stadium;560~Globe Life Park in Arlington;650~Chase Field;580~Petco Park;830')");
-		qDebug() << query.exec("insert into stadiums values(21, 'Dodger Stadium', 'Los Angeles Dodgers', '1000 Elysian Park Avenue', 'Los Angeles', 'CA', '90090', '323-224-1507', '1962-04-10', '56,000', 'National', 'Grass', 'Angels Stadium of Anaheim;0~Target Field;1500~O.co Coliseum;340')");
-		qDebug() << query.exec("insert into stadiums values(22, 'Great American Ball Park', 'Cincinnati Reds', '100 Joe Nuxhall Way', 'Cincinnati', 'OH', '45202', '513-381-7337', '2003-03-31', '42,319', 'National', 'Grass', 'Progressive Field;225~PNC Park;260~Tropicana Field;790~Turner Field;375~Busch Stadium;310~US Cellular Field;250')");
-		qDebug() << query.exec("insert into stadiums values(23, 'Marlins Park', 'Miami Marlins', '501 Marlins Way', 'Miami', 'FL', '33125', '305-480-1300', '2012-04-4', '37,442', 'National', 'Grass', 'Nationals Park;930~Fenway Park;1255~Minute Maid Park;965~Tropicana Field;210~Turner Field;600')");
-		qDebug() << query.exec("insert into stadiums values(24, 'Miller Park', 'Milwaukee Brewers', '1 Brewers Way', 'Milwaukee', 'WI', '53214', '414-902-4400', '2001-04-6', '41,900', 'National', 'Grass', 'Rogers Centre;430~Wrigley Field;80~Target Field;300')");
-		qDebug() << query.exec("insert into stadiums values(25, 'Nationals Park', 'Washington Nationals', '1500 S Capitol St SE', 'Washington', 'DC', '20003', '202-675-6287', '2008-03-30', '41,418', 'National', 'Grass', 'Oriole Park at Camden Yards;0~Marlins Park;930~Turner Field;560~PNC Park;195')");
-		qDebug() << query.exec("insert into stadiums values(26, 'Petco Park', 'San Diego Padres', '19 Tony Gwynn Drive', 'San Diego', 'CA', '92101', '619-795-5000', '2004-04-8', '42,524', 'National', 'Grass', 'Angels Stadium of Anaheim;110~Coors Field;830~Chase Field;300')");
-		qDebug() << query.exec("insert into stadiums values(27, 'PNC Park', 'Pittsburgh Pirates', '115 Federal St', 'Pittsburgh', 'PA', '15212', '412-321-2827', '2001-03-31', '38,362', 'National', 'Grass', 'Rogers Centre;225~Yankee Stadium;315~Nationals Park;195~Great American Ball Park;260~Progressive Field;115')");
-		qDebug() << query.exec("insert into stadiums values(28, 'Turner Field', 'Atlanta Braves', '755 Hank Aaron Drive', 'Atlanta', 'GA', '30315', '404-522-7630', '1997-03-29', '49,586', 'National', 'Grass', 'Great American Ball Park;375~Nationals Park;560~Marlins Park;600~Globe Life Park in Arlington;740')");
-		qDebug() << query.exec("insert into stadiums values(29, 'Wrigley Field', 'Chicago Cubs', '1060 West Addison Street', 'Chicago', 'IL', '60613', '773-404-2827', '1914-04-23', '42,374', 'National', 'Grass', 'US Cellular Field;0~Kauffman Stadium;415~Miller Park;80')");
+							   "'Texas Rangers', '1000 Ballpark Way', "
+							   "'Arlington', 'TX', '76011', '817-273-5222', "
+							   "'1994-04-1', '48,114', 'American', 'Grass', "
+							   "'Chase Field;870~Coors Field;650~Kauffman "
+							   "Stadium;460~Turner Field;740~Minute "
+							   "Maid Park;230')");
+		qDebug() << query.exec("insert into stadiums values"
+							   "(5, 'Kauffman Stadium', 'Kansas City Royals', "
+							   "'1 Royal Way', 'Kansas City', 'MO', '64129', "
+							   "'816-921-8000', '1973-04-10', '37,903', "
+							   "'American', 'Grass', 'Wrigley Field;415~Busch "
+							   "Stadium;235~Globe Life Park in Arlington;"
+							   "460~Coors Field;560')");
+		qDebug() << query.exec("insert into stadiums values"
+							   "(6, 'Minute Maid Park', 'Houston Astros', "
+							   "'501 Crawford St', 'Houston', 'TX', '77002', "
+							   "'713-259-8000', '2000-03-30', '42,060', "
+							   "'American', 'Grass', "
+							   "'Busch Stadium;680~Tropicana Field;790~Marlins "
+							   "Park;965~Chase Field;1115~Globe Life Park in "
+							   "Arlington;230')");
+		qDebug() << query.exec("insert into stadiums values"
+							   "(7, 'O.co Coliseum', 'Oakland Athletics', "
+							   "'7000 Coliseum Way', 'Oakland', 'CA', '94621', "
+							   "'510-569-2121', '1966-09-18', '37,090 (April - "
+							   "August) & 55,945 (Sept - Jan)', 'American', "
+							   "'Grass', 'AT&T Park;0~Chase Field;650~Dodger "
+							   "Stadium;340')");
+		qDebug() << query.exec("insert into stadiums values(8, 'Oriole Park at"
+							   " Camden Yards', 'Baltimore Orioles', '333 West"
+							   " Camden Street', 'Baltimore', 'MD', '21201', "
+							   "'410-685-9800', '1992-04-6', '48,187', "
+							   "'American', 'Grass', 'Nationals Park;"
+							   "0~Citizens Bank Park;90')");
+		qDebug() << query.exec("insert into stadiums values"
+							   "(9, 'Progressive Field', 'Cleveland Indians', "
+							   "'2401 Ontario Street', 'Cleveland', 'OH', "
+							   "'44115', '216-420-4487', '1994-04-2', "
+							   "'42,404', 'American', 'Grass', 'Comerica "
+							   "Park;90~PNC Park;115~Great American Ball Park;"
+							   "225')");
+		qDebug() << query.exec("insert into stadiums values"
+							   "(10, 'Rogers Centre', 'Toronto Blue Jays', "
+							   "'1 Blue Jays Way', 'Toronto', 'Ontario,Canada',"
+							   " 'M5V1J3', '416-341-1000', '1989-06-3', "
+							   "'49,282', 'American', 'Turf', 'Fenway Park;"
+							   "430~PNC Park;225~Comerica Park;210~Miller Park;"
+							   "430~SafeCo Field;2070')");
+		qDebug() << query.exec("insert into stadiums values"
+							   "(11, 'SafeCo Field', 'Seattle Mariners', '1516 "
+							   "First Avenue South', 'Seattle', 'WA', '98134', "
+							   "'206-346-4000', '1999-07-15', '47,476', "
+							   "'American', 'Grass', 'Rogers Centre;2070~Target"
+							   " Field;1390~AT&T Park;680')");
+		qDebug() << query.exec("insert into stadiums values"
+							   "(12, 'Target Field', 'Minnesota Twins', '353 N "
+							   "5th St', 'Minneapolis', 'MN', '55403', "
+							   "'800-338-9467', '2010-04-12', '39,021', "
+							   "'American', 'Grass', 'SafeCo Field;1390~Miller"
+							   " Park;300~Busch Stadium;465~Dodger "
+							   "Stadium;1500')");
+		qDebug() << query.exec("insert into stadiums values"
+							   "(12, 'Tropicana Field', 'Tampa Bay Rays', "
+							   "'1 Tropicana Dr', 'St. Petersburg', 'FL', "
+							   "'33705', '727-825-3137', '1990-03-3', "
+							   "'31,042 (Regular Season) 42,735 (Postseason)',"
+							   " 'American', 'Turf', 'Marlins Park;210~Minute"
+							   " Maid Park;790~Great American Ball Park;790')");
+		qDebug() << query.exec("insert into stadiums values"
+							   "(13, 'US Cellular Field', 'Chicago White Sox',"
+							   " '333 West 35th Street', 'Chicago', 'IL', "
+							   "'60616', '312-674-1000', '1991-04-18', "
+							   "'40,615', 'American', 'Grass', 'Wrigley "
+							   "Field;0~Comerica Park;240~Great American"
+							   " Ball Park;250')");
+		qDebug() << query.exec("insert into stadiums values"
+							   "(14, 'Yankee Stadium', 'New York Yankees', "
+							   "'1 E 161st St', 'South Bronx', 'NY', '10451', "
+							   "'718-293-4300', '2009-04-16', '50,291', "
+							   "'American', 'Grass', 'Citi Field;0~Citizens "
+							   "Bank Park;80~PNC Park;315~Fenway Park;195')");
+		qDebug() << query.exec("insert into stadiums values"
+							   "(15, 'AT&T Park', 'San Francisco Giants', "
+							   "'24 Willie Mays Plaza', 'San Francisco', "
+							   "'CA', '94107', '415-972-2000', '2000-04-11', "
+							   "'41,915', 'National', 'Grass', 'O.co "
+							   "Coliseum;0~SafeCo Field;680')");
+		qDebug() << query.exec("insert into stadiums values"
+							   "(16, 'Busch Stadium', 'St. Louis Cardinals', "
+							   "'700 Clark Street', 'St. Louis', 'MO', "
+							   "'63102', '314-345-9600', '2006-04-10', "
+							   "'46,861', 'National', 'Grass', 'Great American"
+							   " Ball Park;310~Minute Maid Park;680~Kauffman Stadium;235~Target Field;465')");
+		qDebug() << query.exec("insert into stadiums values"
+							   "(17, 'Chase Field', 'Arizona Diamondbacks', '401 East Jefferson Street', 'Phoenix', 'AZ', '85004', '602-462-6500', '1998-03-31', '48,633', 'National', 'Grass', 'Coors Field;580~Globe Life Park  in Arlington;870~Minute Maid Park;1115~Petco Park;300~O.co Coliseum;650')");
+		qDebug() << query.exec("insert into stadiums values"
+							   "(18, 'Citi Field', 'New York Mets', '126th St. & Roosevelt Ave.', 'Queens', 'NY', '11368', '718-507-6387', '2009-04-13', '41,922', 'National', 'Grass', 'Yankee Stadium;0')");
+		qDebug() << query.exec("insert into stadiums values"
+							   "(19, 'Citizens Bank Park', 'Philadelphia Phillies', '1 Citizens Bank Way', 'Philadelphia', 'PA', '19148', '215-463-1000', '2004-04-3', '43,651', 'National', 'Grass', 'Yankee Stadium;80~Oriole Park at Camden Yards;90')");
+		qDebug() << query.exec("insert into stadiums values"
+							   "(20, 'Coors Field', 'Colorado Rockies', '2001 Blake St', 'Denver', 'CO', '80205', '303-292-0200', '1995-04-26', '50,480', 'National', 'Grass', 'Kauffman Stadium;560~Globe Life Park in Arlington;650~Chase Field;580~Petco Park;830')");
+		qDebug() << query.exec("insert into stadiums values"
+							   "(21, 'Dodger Stadium', 'Los Angeles Dodgers', '1000 Elysian Park Avenue', 'Los Angeles', 'CA', '90090', '323-224-1507', '1962-04-10', '56,000', 'National', 'Grass', 'Angels Stadium of Anaheim;0~Target Field;1500~O.co Coliseum;340')");
+		qDebug() << query.exec("insert into stadiums values"
+							   "(22, 'Great American Ball Park', 'Cincinnati Reds', '100 Joe Nuxhall Way', 'Cincinnati', 'OH', '45202', '513-381-7337', '2003-03-31', '42,319', 'National', 'Grass', 'Progressive Field;225~PNC Park;260~Tropicana Field;790~Turner Field;375~Busch Stadium;310~US Cellular Field;250')");
+		qDebug() << query.exec("insert into stadiums values"
+							   "(23, 'Marlins Park', 'Miami Marlins', '501 Marlins Way', 'Miami', 'FL', '33125', '305-480-1300', '2012-04-4', '37,442', 'National', 'Grass', 'Nationals Park;930~Fenway Park;1255~Minute Maid Park;965~Tropicana Field;210~Turner Field;600')");
+		qDebug() << query.exec("insert into stadiums values"
+							   "(24, 'Miller Park', 'Milwaukee Brewers', '1 Brewers Way', 'Milwaukee', 'WI', '53214', '414-902-4400', '2001-04-6', '41,900', 'National', 'Grass', 'Rogers Centre;430~Wrigley Field;80~Target Field;300')");
+		qDebug() << query.exec("insert into stadiums values"
+							   "(25, 'Nationals Park', 'Washington Nationals', "
+							   "'1500 S Capitol St SE', 'Washington', 'DC', "
+							   "'20003', '202-675-6287', '2008-03-30', "
+							   "'41,418', 'National', 'Grass', 'Oriole Park at "
+							   "Camden Yards;0~Marlins Park;930~Turner "
+							   "Field;560~PNC Park;195')");
+		qDebug() << query.exec("insert into stadiums values"
+							   "(26, 'Petco Park', 'San Diego Padres', "
+							   "'19 Tony Gwynn Drive', 'San Diego', 'CA', "
+							   "'92101', '619-795-5000', '2004-04-8', '42,524',"
+							   " 'National', 'Grass', 'Angels Stadium of "
+							   "Anaheim;110~Coors Field;830~Chase Field;300')");
+		qDebug() << query.exec("insert into stadiums values"
+							   "(27, 'PNC Park', 'Pittsburgh Pirates', "
+							   "'115 Federal St', 'Pittsburgh', 'PA', '15212', "
+							   "'412-321-2827', '2001-03-31', '38,362', "
+							   "'National', 'Grass', 'Rogers Centre;225~Yankee "
+							   "Stadium;315~Nationals Park;195~Great American "
+							   "Ball Park;260~Progressive Field;115')");
+		qDebug() << query.exec("insert into stadiums values"
+							   "(28, 'Turner Field', 'Atlanta Braves', "
+							   "'755 Hank Aaron Drive', 'Atlanta', 'GA', "
+							   "'30315', '404-522-7630', '1997-03-29', "
+							   "'49,586', 'National', 'Grass', 'Great American "
+							   "Ball Park;375~Nationals Park;560~Marlins Park;"
+							   "600~Globe Life Park in Arlington;740')");
+		qDebug() << query.exec("insert into stadiums values"
+							   "(29, 'Wrigley Field', 'Chicago Cubs', "
+							   "'1060 West Addison Street', 'Chicago', 'IL', "
+							   "'60613', '773-404-2827', '1914-04-23', "
+							   "'42,374', 'National', 'Grass', 'US Cellular "
+							   "Field;0~Kauffman Stadium;415~Miller Park;80')");
 	}
-
 
 	return tableMade;
 }
